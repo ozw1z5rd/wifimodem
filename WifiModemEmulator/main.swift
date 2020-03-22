963dff39d845beabede0125b7fdc7f361d4a6933
ozw1z5rd <ozw1z5rd@gmail.com>
2020-03-22 17:11:12 +0100

    Initial Commit

---
+++ WifiModemEmulator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WifiModemEmulator
+//
+//  Created by bigfoot on 22/03/2020.
+//  Copyright © 2020 AlessioPalma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
